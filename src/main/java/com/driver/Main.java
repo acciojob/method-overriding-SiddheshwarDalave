1f4d5b4e98dc624070b454774b2dfcbafe73a738
Siddheshwar <siddheshwardalave9119@gmail.com>
2022-10-11 08:30:04 +0530

    Method overriding commit2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,12 +7,12 @@ public class Main {
   }
 }
 class A{
-    String meth(){
+    public String meth(){
       return "Invoking method from class A";
     }
 }
 class B extends A{
-    String meth(){
+    public String meth(){
         return "Method is overridden in Extendend class B";
     }
 }
